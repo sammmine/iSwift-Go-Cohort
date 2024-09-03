ddba6c8c27df474e20f54d9634c569086d54093c
Samantha Laqueenna Ginting <167011131+sammmine@users.noreply.github.com>
2024-09-03 22:09:31 +0700

    feat: updating arranged.go

--- mini-challenge-4/arranged.go
+++ mini-challenge-4/arranged.go
@@ -5,32 +5,39 @@ import (
 	"sync"
 )
 
-type dataa interface {
+// Untuk yang arranged, saya tidak mengerti jika menggunakan tipe data interface.
+// Sehingga di sini untuk kedua datanya saya memakai tipe data struct.
+// Isi dari struct adalah slice of string (untuk datanya) dan sync.Mutex.
+type data struct {
+	data  []string
+	mutex sync.Mutex
 }
 
-func rutinn(slice dataa, i int, wg *sync.WaitGroup, mutex *sync.Mutex) {
-	defer wg.Done()
-	mutex.Lock()
-	fmt.Println(slice, i)
-	mutex.Unlock()
+// Method ini untuk menampilkan/mengoutput isi struct.
+// Mutex diimplementasikan agar output terurut (atau synchronize).
+func (d *data) getData() string {
+	d.mutex.Lock()
+	defer d.mutex.Unlock()
+	return fmt.Sprintf("[%s %s %s]", d.data[0], d.data[1], d.data[2])
 }
 
 func main() {
-	var mutex sync.Mutex
+	// Pertama kita isi kedua buah data yang bertipe struct.
+	slice1 := &data{data: []string{"coba1", "coba2", "coba3"}}
+	slice2 := &data{data: []string{"bisa1", "bisa2", "bisa3"}}
+
 	var wg sync.WaitGroup
 
-	var slice1 dataa = []dataa{"coba1", "coba2", "coba3"}
-	var slice2 dataa = []dataa{"bisa1", "bisa2", "bisa3"}
+	wg.Add(1)
 
-	wg.Add(8)
-
-	for i := 1; i < 5; i++ {
-		mutex.Lock()
-		go rutinn(slice1, i, &wg, &mutex)
-		go rutinn(slice2, i, &wg, &mutex)
-		mutex.Unlock()
-	}
+	// Implementasi concurrency di sini saya hanya menggunakan satu goroutine.
+	go func() {
+		defer wg.Done()
+		for i := 1; i <= 4; i++ {
+			fmt.Println(slice1.getData(), i)
+			fmt.Println(slice2.getData(), i)
+		}
+	}()
 
 	wg.Wait()
-
 }
